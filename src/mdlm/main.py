664a72bab1f9f50be5783d75402c1d0623b737a5
hrim <commit.im@gmail.com>
2026-04-19 16:47:57 +0900

    add source code: MDLM

--- src/mdlm/main.py
+++ src/mdlm/main.py
@@ -4,7 +4,7 @@ from raenimgl import *
 
 class ARModel(InteractiveScene, Scene2D):
     def construct(self):
-        # model
+        ## model
 
         model_box = Rectangle(
             width=4, height=2, stroke_color=GREY_A, fill_color=GREY_C, fill_opacity=1
@@ -17,7 +17,7 @@ class ARModel(InteractiveScene, Scene2D):
         model = VGroup(model_box, modelt).set_z_index(2)
         self.playw(FadeIn(model))
 
-        # words
+        ## words
         full_sentence = (
             "4월 초 샌프란시스코랑 LA 날씨를 검색해서 알려줘. </prompt> "
             + "4월 초 기준 샌프란시스코와 LA의 날씨는 다음과 같습니다. ..."
@@ -34,7 +34,7 @@ class ARModel(InteractiveScene, Scene2D):
             lag_ratio=0.2,
         )
 
-        # prompt -> model -> output
+        ## prompt -> model -> output
         self.play(prompt[-1].animate.set_opacity(1), run_time=0.5)
         self.playw(Restore(prompt))
         model_input = prompt.copy()
@@ -57,7 +57,7 @@ class ARModel(InteractiveScene, Scene2D):
         )
         self.playw(FlashAround(answer[0], buff=0.05), FlashAround(px[2:4], buff=0.05))
 
-        # iteration
+        ## iteration
         for i in range(1, len(answer)):
             model_input = VGroup(prompt, answer[:i]).copy()
             self.play(
@@ -73,7 +73,7 @@ class ARModel(InteractiveScene, Scene2D):
                 run_time=0.5,
             )
         self.wait()
-        # flash each word
+        ## flash each word
         self.playwl(
             *[FlashAround(w, buff=0.05, color=GREEN) for w in answer], lag_ratio=0.3
         )
@@ -82,7 +82,7 @@ class ARModel(InteractiveScene, Scene2D):
 class MDLModel(InteractiveScene, Scene2D):
     def construct(self):
 
-        # model
+        ## model
         model_box = Rectangle(
             width=4, height=2, stroke_color=GREY_A, fill_color=GREY_C, fill_opacity=1
         ).shift(UP)
@@ -99,7 +99,7 @@ class MDLModel(InteractiveScene, Scene2D):
         model = VGroup(model_box, modelt).set_z_index(2)
         self.playw(FadeIn(model))
 
-        # words
+        ## words
         full_sentence = (
             "4월 초 샌프란시스코랑 LA 날씨를 검색해서 알려줘. </prompt> "
             + "4월 초의 샌프란시스코와 LA의 날씨는 다음과 같습니다. ..."
@@ -124,15 +124,24 @@ class MDLModel(InteractiveScene, Scene2D):
         self.playw(FadeIn(prompt))
         self.playw(FadeIn(masked))
 
-        # a single diffusion step
+        ## a single diffusion step
         random_idx = list(range(len(words.words) - start_idx))
         random.shuffle(random_idx)
         idx_align = {idx: i for i, idx in enumerate(random_idx)}
 
+        px = Tex(r"p(", "x_{t-1}", "|", r"x_{t}", ")", font_size=28).next_to(
+            model_box, RIGHT, buff=0.5
+        )
+        self.playw(FadeIn(px, shift=RIGHT * 0.5))
+
         answer_unit = 2
 
         model_input = VGroup(prompt, masked).copy()
-        self.play(model_input.animate.scale(0.3).move_to(model_box.get_center()))
+        self.play(
+            model_input.animate.scale(0.3).move_to(model_box.get_center()),
+            FlashAround(px[-4:-1], buff=0.05),
+            run_time=0.5,
+        )
         answer = words.words[start_idx:]
         for i, a in enumerate(answer):
             a.rotate(PI / 2).move_to(masked[i].get_center())
@@ -151,13 +160,14 @@ class MDLModel(InteractiveScene, Scene2D):
             lag_ratio=0.3,
         )
 
-        # iteration
+        ## iteration
         answers = VGroup(*answer0)
         mask_remain = VGroup(*[masked[i] for i in random_idx[answer_unit:]])
         for i in range(1, len(answer) // answer_unit):
             model_input = VGroup(prompt, answers, mask_remain).copy()
             self.play(
                 model_input.animate.scale(0.3).move_to(model_box.get_center()),
+                FlashAround(px[-4:-1], buff=0.05),
                 run_time=0.5,
             )
             answeri = map(
@@ -187,7 +197,7 @@ class MDLModel(InteractiveScene, Scene2D):
         self.wait()
         self.embed()
 
-        # flashing units
+        ## flashing units
         """
         answer0 = VGroup(*[answer[i] for i in random_idx[:answer_unit]]).set_color(GREEN)
         answer1 = VGroup(*[answer[i] for i in random_idx[answer_unit:2*answer_unit]]).set_color(GREEN)
@@ -222,6 +232,9 @@ class MDLModel(InteractiveScene, Scene2D):
             ],
             lag_ratio=0.9,
         )
+        ol = self.overlay
+        xt = px[2:6].set_z_index(ol.z_index + 1)
+        self.playw(xt.animate.shift(UP * 0.5).set_color(RED), FadeIn(ol))
 
 
 class MDLModel_good(InteractiveScene, Scene2D):
@@ -236,7 +249,7 @@ class MDLModel_good(InteractiveScene, Scene2D):
 
         self.playw(FadeIn(words))
 
-        # generation order in AR
+        ## generation order in AR
         ar_text = (
             Text('" Autoregressive model "')
             .scale(0.5)
@@ -248,7 +261,7 @@ class MDLModel_good(InteractiveScene, Scene2D):
             self.play(FlashAround(words.words[i], buff=0.05, color=GREEN), run_time=0.7)
         self.wait()
 
-        # generation order in MDL
+        ## generation order in MDL
         mdl_text = (
             Text('" Masked Diffusion Language model "')
             .scale(0.5)
@@ -268,11 +281,11 @@ class MDLModel_good(InteractiveScene, Scene2D):
             self.play(*anims, run_time=0.7)
         self.wait()
 
-        # fadeout text
+        ## fadeout text
         self.playw(FadeOut(mdl_text, shift=UP * 0.5))
 
         self.embed()
-        # brace length
+        ## brace length
         brace = Brace(words.words[start_idx:], UP, buff=0.2).set_color(GREY_B)
         brace_text = (
             Tex(r"\text{length} = " + str(len(words.words) - start_idx))
@@ -282,7 +295,7 @@ class MDLModel_good(InteractiveScene, Scene2D):
         self.play(FadeIn(brace))
         self.playw(FadeIn(brace_text))
 
-        # # of calls
+        ## ## of calls
         ar_calls = (
             Words("Autoregressive: " + str(len(words.words) - start_idx))
             .scale(0.4)
@@ -324,7 +337,7 @@ class MDLModel_good(InteractiveScene, Scene2D):
 
 class ARModel_detail(InteractiveScene, Scene2D):
     def construct(self):
-        # model
+        ## model
         model_box = Rectangle(
             width=9, height=2, stroke_color=GREY_A, fill_color=GREY_C, fill_opacity=0.7
         ).shift(UP)
@@ -336,7 +349,7 @@ class ARModel_detail(InteractiveScene, Scene2D):
         model = VGroup(model_box, modelt).set_z_index(2)
         self.playw(FadeIn(model))
 
-        # words
+        ## words
         full_sentence = (
             "4월 초 샌프란시스코랑 LA 날씨를 검색해서 알려줘. </prompt> "
             + "4월 초 기준 샌프란시스코와 ..."
@@ -347,7 +360,7 @@ class ARModel_detail(InteractiveScene, Scene2D):
         prompt = words.words[:start_idx]
         self.playwl(FadeIn(prompt, shift=UP * 0.3), lag_ratio=0.2, wait=0)
 
-        # rotate and arrange
+        ## rotate and arrange
         prompt.generate_target()
         for w in prompt.target:
             w.rotate(PI / 2)
@@ -356,7 +369,7 @@ class ARModel_detail(InteractiveScene, Scene2D):
         ).align_to(prompt, UL)
         self.playw(MoveToTarget(prompt))
 
-        # first model inference
+        ## first model inference
         output = (
             VGroup(
                 *[
@@ -399,7 +412,7 @@ class ARModel_detail(InteractiveScene, Scene2D):
             .set_color(WHITE)
         )
 
-        # iteration
+        ## iteration
         model_input = VGroup(*prompt.copy())
         result = VGroup(*prompt, out_word)
         for idx in range(1, len(words.words) - start_idx):
@@ -459,7 +472,7 @@ class ARModel_detail(InteractiveScene, Scene2D):
         self.wait()
         self.embed()
 
-        # rotate and flash each word
+        ## rotate and flash each word
         result.generate_target()
         for w in result.target:
             w.rotate(-PI / 2)
@@ -478,7 +491,7 @@ class ARModel_detail(InteractiveScene, Scene2D):
             lag_ratio=0.5,
         )
 
-        # chat
+        ## chat
 
         self.play(FadeOut(model), FadeOut(result[start_idx - 1]))
         user_chat = result[: start_idx - 1]
@@ -525,7 +538,7 @@ class ARModel_detail(InteractiveScene, Scene2D):
 
 class MDLModel_detail(InteractiveScene, Scene2D):
     def construct(self):
-        # model
+        ## model
         model_box = Rectangle(
             width=9, height=2, stroke_color=GREY_A, fill_color=GREY_C, fill_opacity=0.7
         ).shift(UP)
@@ -542,7 +555,7 @@ class MDLModel_detail(InteractiveScene, Scene2D):
         model = VGroup(model_box, modelt).set_z_index(2)
         self.playw(FadeIn(model))
 
-        # words
+        ## words
         full_sentence = (
             "4월 초 샌프란시스코랑 LA 날씨를 검색해서 알려줘. </prompt> "
             + "4월 초의 샌프란시스코와 LA의 날씨는 ..."
@@ -567,7 +580,7 @@ class MDLModel_detail(InteractiveScene, Scene2D):
         self.playw(FadeIn(prompt))
         self.playw(FadeIn(masked))
 
-        # rotate and arrange
+        ## rotate and arrange
         total = VGroup(*prompt, *masked)
         total.generate_target()
         for w in total.target:
@@ -577,14 +590,22 @@ class MDLModel_detail(InteractiveScene, Scene2D):
         ).shift(RIGHT * 0.6)
         self.playw(MoveToTarget(total))
 
-        # a single diffusion step
+        ## a single diffusion step
         output = (
             Tensor(len(total), shape="square", arrange=RIGHT, buff=0.36)
             .scale(0.7)
             .next_to(model_box, UP, buff=0.5)
             .align_to(total, LEFT)
         )
+        px = Tex(r"p(", "x_{t-1}", "|", r"x_{t}", ")", font_size=28).next_to(
+            model_box, LEFT, buff=0.35
+        )
+        self.playw(FadeIn(px, shift=LEFT * 0.5))
         self.playw(FadeTransform(total.copy(), output))
+        self.play(FlashAround(px[-4:-1], buff=0.05))
+        self.playw(FlashAround(total))
+        self.play(FlashAround(px[2:6], buff=0.05))
+        self.playw(FlashAround(output))
 
         self.play(FadeOut(output[: len(prompt)], shift=UP * 0.5))
         random_idx = [[1, 4], [0, 2], [3, 5]]
@@ -615,7 +636,7 @@ class MDLModel_detail(InteractiveScene, Scene2D):
         )
 
         self.embed()
-        # iteration
+        ## iteration
 
         for idx_pair in random_idx[1:]:
             model_input = VGroup(*prompt, *masked).copy()
@@ -655,3 +676,155 @@ class MDLModel_detail(InteractiveScene, Scene2D):
                 run_time=0.7,
             )
         self.wait()
+
+
+class MeaningOfT(InteractiveScene, Scene2D):
+    def construct(self):
+        model_box = Rectangle(
+            width=5, height=3, stroke_color=GREY_A, fill_color=GREY_C, fill_opacity=1
+        ).shift(UP)
+        modelt = (
+            Text(
+                "Masked Diffusion Language Model",
+                font="Noto Sans KR",
+                font_size=24,
+                color=BLACK,
+            )
+            .move_to(model_box.get_center())
+            .set_z_index(1)
+        )
+        model = VGroup(model_box, modelt).set_z_index(2)
+        px = Tex(r"p(", "x_{t-1}", "|", r"x_{t}", ")", font_size=36).next_to(
+            model_box, LEFT, buff=0.35
+        )
+        self.addw(model, px)
+
+
+        ## meaning of t 이 과정을 보고 나면은요 ~ 알 수 있습니다
+
+        x_t1 = px[2:6]
+        x_t = px[7:9]
+        self.playw(Indicate(x_t, color=YELLOW), run_time=0.7)
+        self.playw(Indicate(x_t1, color=YELLOW), run_time=0.7)
+
+        ## 결론부터 말하면 이 t는 ~ x_0이라고 주로 표현하죠?
+
+        arr1 = Arrow(
+            x_t1[1].get_bottom() + DOWN * 0.6,
+            x_t1[1].get_bottom(),
+            buff=0.1,
+            color=YELLOW_B,
+            thickness=2,
+        )
+        arr2 = Arrow(
+            x_t[1].get_bottom() + DOWN * 0.6,
+            x_t[1].get_bottom(),
+            buff=0.1,
+            color=YELLOW_B,
+            thickness=2,
+        )
+
+        self.play(
+            GrowArrow(arr1),
+            GrowArrow(arr2),
+            x_t1[1].animate.set_color(YELLOW_B),
+            x_t[1].animate.set_color(YELLOW_B),
+        )
+
+        diff_t = Text(
+            "Diffusion step", font="Noto Sans KR", color=YELLOW_B, font_size=20
+        ).next_to(VGroup(arr1, arr2), DOWN, buff=0.1)
+        self.playw(FadeIn(diff_t, shift=DOWN * 0.1))
+
+        ## DLM에서도 마찬가지로요 ~ 완전한 글이 됩니다
+
+        self.playw(FadeOut(diff_t), FadeOut(arr1), FadeOut(arr2))
+
+        def get_ti(t):
+            t = (
+                Tex("t  =  " + str(t), font_size=36)
+                .set_color(YELLOW_B)
+                .next_to(model_box, LEFT, buff=0.5, aligned_edge=DOWN)
+            )
+            return t
+
+        tT = get_ti("T")
+        self.play(FadeIn(tT, shift=DOWN * 0.3))
+
+        words = Words(
+            "4월 초 샌프란시스코랑 LA 날씨를 검색해서 알려줘. </prompt> 4월 초의 샌프란시스코와 LA의 날씨는 다음과 같습니다. ...",
+            font="Noto Sans KR",
+            font_size=18,
+        ).shift(DOWN)
+        for w in words.words:
+            w.rotate(PI / 2)
+        words.words.arrange(RIGHT, aligned_edge=UP, buff=0.25).next_to(
+            model_box, DOWN, buff=0.5
+        )
+
+        start_idx = 8
+
+        masks = (
+            VGroup(
+                *[
+                    Text("<MASK>", font="Noto Sans KR", font_size=18)
+                    .set_color(RED_B)
+                    .rotate(PI / 2)
+                    .move_to(words.words[i].get_center())
+                    for i in range(start_idx, len(words.words))
+                ]
+            )
+            .arrange(RIGHT, buff=0.25)
+            .next_to(words.words[start_idx - 1], RIGHT, buff=0.25, aligned_edge=UP)
+        )
+
+        self.playw(FadeIn(words.words[:start_idx]), FadeIn(masks))
+        masks_ = masks.copy()
+        words_ = words.copy(deep=True)
+        t0 = get_ti(0)
+        self.playw(
+            Transformr(tT, t0),
+            Transformr(masks, words.words[start_idx:]),
+        )
+        
+        ## 그리고 저번 영상에서 말한 ~ transition입니다
+        tT = get_ti("T")
+        self.playw(
+            Transformr(t0, tT),
+            Transformr(words.words[start_idx:], masks_),
+        )
+        self.embed()
+
+        ##  정리하면, ~ 진행됩니다
+        order = [[6, 1], [0, 2], [3, 5], [4, 7]]
+        tT1 = get_ti("T-1")
+        words_to_show = VGroup(*[words_.words[start_idx + i] for i in order[0]])
+        mask_to_show = VGroup(*[masks_[i] for i in order[0]])
+        self.playw(
+            Transformr(tT, tT1),
+            Transformr(mask_to_show, words_to_show),
+        )
+
+        tT2 = get_ti("T-2")
+        words_to_show2 = VGroup(*[words_.words[start_idx + i] for i in order[1]])
+        mask_to_show2 = VGroup(*[masks_[i] for i in order[1]])
+        self.play(
+            Transformr(tT1, tT2),
+            Transformr(mask_to_show2, words_to_show2),
+        )
+
+        tTdot = get_ti("...")
+        words_to_show3 = VGroup(*[words_.words[start_idx + i] for i in order[2]])
+        mask_to_show3 = VGroup(*[masks_[i] for i in order[2]])
+        self.playw(
+            Transformr(tT2, tTdot),
+            Transformr(mask_to_show3, words_to_show3),
+        )
+
+        tT0 = get_ti(0)
+        words_to_show4 = VGroup(*[words_.words[start_idx + i] for i in order[3]])
+        mask_to_show4 = VGroup(*[masks_[i] for i in order[3]])
+        self.play(
+            Transformr(tTdot, tT0),
+            Transformr(mask_to_show4, words_to_show4),
+        )
