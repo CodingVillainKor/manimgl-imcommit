e8b4edb1f74775ad47a947f490da54231ecc7c70
hrim <commit.im@gmail.com>
2026-04-19 16:48:26 +0900

    double #

--- _mhelper.py
+++ _mhelper.py
@@ -13,7 +13,7 @@ with open(_f, encoding="utf-8") as _fh:
     _lines = _fh.readlines()
 
 for _i in range(len(_lines) - 1, -1, -1):
-    if _lines[_i].strip().startswith("#"):
+    if _lines[_i].strip().startswith("##"):
         pyperclip.copy("".join(_lines[_i:]))
         break
 
