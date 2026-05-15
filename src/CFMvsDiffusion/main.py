c54d314b1d1f1282861a54ac4415f4bad0b5dde3
Imcommit <commit.im@gmail.com>
2026-05-15 15:40:40 +0900

    making paperFigure

--- src/CFMvsDiffusion/main.py
+++ src/CFMvsDiffusion/main.py
@@ -5,24 +5,12 @@ from random import seed
 seed(41)
 np.random.seed(41)
 
-class paperFigure(InteractiveScene, Scene2D):
-    def construct(self):
-        self.embed()
-
-        ## ## Scene 1
-        """
-        **핵심: Flow matching 논문에서의 비교 figure**
-        1. 직선 궤적을 따라가는 flow matching
-        2. 약간 돌아가는 궤적을 그리는 diffusion
-        3. 정말 그럴까?
-        """
-
 
 class equation(InteractiveScene, Scene2D):
     def construct(self):
         self.embed()
 
-        ## 
+        ## Scene 2
         """
         ## Scene 2
         **핵심: 수식과 코드를 자세히 보자**
@@ -43,3 +31,39 @@ class actually(InteractiveScene, Scene2D):
         2. Diffusion: 목적지를 예측
         3. Flow matching: 목적지 방향을 예측
         """
+
+
+class paperFigure(InteractiveScene, Scene2D):
+    def construct(self):
+        self.embed()
+
+        """
+        **핵심: Flow matching 논문에서의 비교 figure**
+        1. 약간 돌아가는 궤적을 그리는 diffusion
+        2. 직선 궤적을 따라가는 flow matching
+        3. 정말 그럴까?
+        """
+        ## Scene 1
+        cfm_fig = ImageMobject("cfm_fig.png").scale(0.75)
+        self.playw(FadeIn(cfm_fig))
+        
+        nump_diff = RaenimPlane()
+        nump_cfm = RaenimPlane()
+
+        VGroup(nump_diff, nump_cfm).arrange(RIGHT, buff=1.5)
+
+        start_diff_arr = np.array([1.5, 1.5, 0])
+        start_cfm_arr = np.array([1.5, 1.5, 0])
+        end_diff_arr = np.array([-1.5, -1.5, 0])
+        end_cfm_arr = np.array([-1.5, -1.5, 0])
+        
+        start_diff_dot = Dot(start_diff_arr, color=RED)
+        start_cfm_dot = Dot(start_cfm_arr, color=RED)
+        end_diff_dot = Dot(end_diff_arr, color=GREEN)
+        end_cfm_dot = Dot(end_cfm_arr, color=GREEN)
+        
+
+
+        # diffusion_fig = ImageMobject("diffusion_paper.png").scale(0.75)
+        # self.playw(FadeIn(diffusion_fig))
+
